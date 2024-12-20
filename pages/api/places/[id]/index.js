e2a7e07e0cf73f69b172bb0355f4dd31e2091653
Maria Osypova <mosipova1998@gmail.com>
2024-12-20 16:05:06 +0100

    connect with MpngoBD and add GET

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,17 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    response.status(200).json(place);
+    if (!place) {
+      response.status(404).json({ status: "Not found" });
+      return;
+    }
+    response.status(200).json(place);
   }
-
-  response.status(200).json(place);
 }
