bfbb126d971616f6a3a9111aa588964618327ae1
Maria Osypova <mosipova1998@gmail.com>
2024-12-20 16:50:26 +0100

    add 50% of Create

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,11 +4,23 @@ import Place from "@/db/models/Place";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const places = await Place.find();
-    console.log("Places: ", places);
-    response.status(200).json(places);
-    return;
+  try {
+    if (request.method === "GET") {
+      const places = await Place.find();
+      console.log("Places: ", places);
+      response.status(200).json(places);
+      return;
+    }
+
+    if (request.method === "POST") {
+      const placeData = request.body;
+      await Place.create(placeData);
+      response.status(201).json({ status: "Place created!" });
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ status: "Oops, something went wrong" });
   }
   response.status(405).json({ status: "Method not allowed." });
 }
